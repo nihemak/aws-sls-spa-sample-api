80437c875cd315601c4bc61302da81195bab69f3
Makoto Nihei <nihemak@gmail.com>
2018-08-25 10:05:41 +0900

    Add update handler of todo

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -94,3 +94,58 @@ export const get: Handler = (
       });
     });
 };
+
+export const update: Handler = (
+  event: APIGatewayEvent,
+  _: Context,
+  cb: Callback
+) => {
+  if (!event.pathParameters) {
+    console.error("Validation Failed");
+    cb(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Couldn't update the todo item. id"
+      })
+    });
+    return;
+  }
+  const input = JSON.parse(event.body || "{}");
+  if (!input.text || validator.isEmpty(input.text)) {
+    console.error("Validation Failed");
+    cb(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Couldn't update the todo item. text"
+      })
+    });
+    return;
+  }
+  if (!input.checked || !validator.isBoolean(input.checked)) {
+    console.error("Validation Failed");
+    cb(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Couldn't update the todo item. checked"
+      })
+    });
+    return;
+  }
+  new Todos()
+    .update(event.pathParameters.id, input.text, input.checked)
+    .then(todo => {
+      cb(null, {
+        statusCode: 200,
+        body: JSON.stringify(todo)
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      cb(null, {
+        statusCode: error.statusCode || 501,
+        body: JSON.stringify({
+          message: "Couldn't update the todo item."
+        })
+      });
+    });
+};
