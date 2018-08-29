24ccbb43480fc7a18143bd20ec21490c918bc52e
Makoto Nihei <nihemak@gmail.com>
2018-08-30 01:18:09 +0900

    Fix tslint error

--- src/commands/CreateLocalTables.ts
+++ src/commands/CreateLocalTables.ts
@@ -1,10 +1,10 @@
 import { Todos } from "../models/Todos";
 
 new Todos()
-.createTable(1 /* readCapacity */, 1 /* writeCapacity */)
-.then(() => {
-  console.log("Tables has been created");
-})
-.catch(error => {
-  console.error("Error creating tables: " + error);
-});
+  .createTable(1 /* readCapacity */, 1 /* writeCapacity */)
+  .then(() => {
+    console.log("Tables has been created");
+  })
+  .catch(error => {
+    console.error("Error creating tables: " + error);
+  });
