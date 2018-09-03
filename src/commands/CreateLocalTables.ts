5c96539594a8fa6264271ae095a73d8180979290
Makoto Nihei <nihemak@gmail.com>
2018-09-04 01:15:21 +0900

    Use InversifyJS to models

--- src/commands/CreateLocalTables.ts
+++ src/commands/CreateLocalTables.ts
@@ -1,6 +1,8 @@
+import { container, TYPES } from "../providers/inversify.config";
 import { Todos } from "../models/Todos";
 
-new Todos()
+container
+  .get<Todos>(TYPES.Todos)
   .createTable(1 /* readCapacity */, 1 /* writeCapacity */)
   .then(() => {
     console.log("Tables has been created");
