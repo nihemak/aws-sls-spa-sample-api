dc3684a29e1e07c662c7f62f88c3d0b54193dba8
Makoto Nihei <nihemak@gmail.com>
2018-09-10 23:58:45 +0900

    Remove unused @types/dynamodb.d.ts

--- src/@types/dynamodb.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "dynamodb" {
-  var types: any;
-  function dynamoDriver(driver: any): any;
-  function define(modelName: string, config: any): any;
-  function createTables(options: any, callback: any): any;
-}
